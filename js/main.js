7c05a6ca20cc94af19146198592ed3b7626a311e
MattiaGalasso <galasso.mattia94@gmail.com>
2025-12-19 15:03:00 +0100

    Add JavaScript First Exercise

--- js/main.js
+++ js/main.js
@@ -11,6 +11,28 @@ stampa un messaggio appropriato sull’esito del controllo, utilizzando un ciclo
 Non è necessario provvedere alla validazione delle email
 */
 
+// Lista di mail
+const guestsList = ["mail1", "mail2"];
+
+// Chiedo all'utente la sua mail
+const userMail = prompt("Inserisci la mail");
+
+// Controllo se la mail è presente nella Array
+let message = `La ${userMail} inserita non è presente nella lista degli invitati!`;
+let mailPresent = false;
+
+for (i = 0; i < guestsList.length; i++) {
+  const currentList = guestsList[i];
+
+  if (userMail === currentList) {
+    message = `La ${userMail} è presente nella lista degli invitati! Buon Divertimento!`;
+    mailPresent = true;
+  }
+}
+
+console.log("Mail trovata: ", mailPresent);
+alert(message);
+
 /*
 
 
